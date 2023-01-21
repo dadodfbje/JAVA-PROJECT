c8a3a3ad77f53f3db2cc68ee41ac407a7cf58986
kumar <kumar@DESKTOP-T07I2AT>
2023-01-21 21:01:10 +0530

    add

--- TQtaining/src/assignmentfirst19String/OccuranceOfFirstCharacter.java
+++ TQtaining/src/assignmentfirst19String/OccuranceOfFirstCharacter.java
@@ -7,21 +7,22 @@ public class OccuranceOfFirstCharacter {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the string");
              String s=sc.next();
              int j;
              char ch[]=s.toCharArray();
              for(int i=0;i<ch.length;i++)
              {
-            	 int count=0;
+            	 int count=1;
             	 for(j=i+1;j<ch.length;j++)
             	 {
             		 if(ch[i]==ch[j])
             		 {
-            			 count++;
+            			 count=count+1;
             			 ch[j]=0;
             		 }
             	 }
-            	 if(ch[j]!=0)
+            	 if(ch[i]!=0)
             	 {
             		 System.out.println(ch[i]+"------>"+count);
             	 }
--- TQtaining/src/string/ArrayTOStringLogical.java
+++ TQtaining/src/string/ArrayTOStringLogical.java
@@ -5,11 +5,12 @@ public class ArrayTOStringLogical {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         String arr[]= {"hi","Friend"};
-        String ch=null;
+        String ch="";
+       
         for(int i=0;i<arr.length;i++)
         {
         	ch=ch+arr[i];
-        	//System.out.print(ch);
+        	
         }
      System.out.println(ch);
 	}
