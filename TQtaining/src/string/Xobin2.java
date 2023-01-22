2ff895e9cfd7a681463e3e1a8060881f5cba6d5c
kumar <kumar@DESKTOP-T07I2AT>
2023-01-22 23:27:30 +0530

    add

--- TQtaining/src/string/Xobin2.java
+++ TQtaining/src/string/Xobin2.java
@@ -6,18 +6,20 @@ public class Xobin2 {
 
 	public static void main(String[] args) {
 		
-               String arr= "india is my country";
-               String arr2= "India is best country";                                 
-               String[] a=arr.split("\\s");              
-               System.out.println(Arrays.toString(a));
-               String []b=arr2.split("\\s");
-               
-               System.out.println(Arrays.toString(b));
-               for(int i=0;i<a.length;i++)
-               {
-            	   if(a[i].equals(b[i].toLowerCase()))
+                 String arr= "india is my country and India is best country";
+                                   
+                   String[] a=arr.split("\\s");              
+              
+            
+                
+                for(int i=0;i<a.length;i++)
+               {     
+            	   for(int j=i+1;j<a.length;j++)
+            	   {
+            	   if(a[i].equals(a[j].toLowerCase()))
             	   {
             		  System.out.println(a[i]); 
+            	   }
             	   }
                }
 	}
--- TQtaining/src/string/Xobin3.java
+++ TQtaining/src/string/Xobin3.java
@@ -1,22 +1,53 @@
 package string;
 
+
+
 public class Xobin3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		 String arr= "india is my country";
-         String arr2= "India is best country"; 
-        
-         for(int i=0;i>=0;i++)
-         {
-        	 for(int j=0;j>=0;j++)
-        	 {
-        		 if(arr.charAt(0)==arr2.toLowerCase().charAt(0))
-        		 {
-        			 System.out.println();
-        		 }
-        	 }
-         }
+		
+		 String arr= "india is my country and India is best country";
+		 char ch[]=new char[46];
+		                    
+		 for(int i=0;i<arr.length();i++)
+		 {
+			 ch[i]=arr.charAt(i);
+		    	
+		 }
+		 
+		
+                    
+		  for(int i=0;i<ch.length;i++)
+		  {
+              if(ch[i]>=65 && ch[i]<=90)
+    	      {    
+    		       ch[i]=(char) (ch[i]+32);
+    		         		    
+    	      }
+		  }
+		
+		 String ch1="";
+		 for(int i=0;i<ch.length;i++)
+		 {
+			 ch1=ch1+ch[i]; 
+			
+		 }  
+		     System.out.println(ch1);
+           
+		     String[] a=ch1.split("\\s");              
+             
+	           for(int i=0;i<a.length;i++)
+	           {    
+	        	   for(int j=i+1;j<a.length;j++)
+	        	   {
+	        		   if(a[i].equals(a[j]))
+	        		   {
+	        	          System.out.println(a[i]);
+	        		   }  
+	        	   }
+	           }
+             
+
 	}
 
 }
